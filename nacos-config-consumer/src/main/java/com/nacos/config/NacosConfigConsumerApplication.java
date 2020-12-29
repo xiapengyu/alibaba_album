f03e5867ffa965c94765f8b8755076d85012d613
xiapengyu@vivo.com <xiapengyu@vivo.com>
2020-12-29 14:33:31 +0800

    测试使用nacos配置中心功能的组件

---
+++ nacos-config-consumer/src/main/java/com/nacos/config/NacosConfigConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigConsumerApplication.class, args);
+    }
+
+}
